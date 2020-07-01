b2cc93eb3dc7125c720270d3a4adf6df4c14da5b
diegomestre2 <diegogomest@gmail.com>
2020-07-01 19:58:43 +0200

    Adding more definitions for book

--- src/include/book.hpp
+++ src/include/book.hpp
@@ -4,10 +4,12 @@
 #include <iostream>
 #include <string>
 
+enum class Genre { fiction, nonfiction, periodical, biography, children };
 class Book {
 public:
 	Book() = default;
-	Book(std::string isbn_, std::string title_, std::string author_, std::string copyright_date_) {
+	Book(std::string isbn_, std::string title_, std::string author_, std::string copyright_date_, Genre genre_)
+	    : genre(genre_) {
 		if (not is_valid_isbn(isbn_)) {
 			throw std::invalid_argument("ISBN not valid. Valid format is n-n-n-x");
 		}
@@ -20,34 +22,21 @@ public:
 	}
 	void check_out() {
 	}
-	bool is_valid_isbn(const std::string &isbn) {
-		size_t position = 0;
-		size_t count = 0;
-		for (auto ch : isbn) {
-			count++;
-			if (ch != '-') {
-				auto digit = static_cast<uint32_t>(ch + '0');
-				if ((digit < 0 || digit > 9)) {
-					if (count == 4) {
-						return true;
-					}
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-	const std::string &get_isbn() {
+	bool is_valid_isbn(std::string &isbn);
+	std::string &get_isbn() {
 		return isbn;
 	}
-	const std::string &get_title() {
+	std::string &get_title() {
 		return title;
 	}
-	const std::string &get_author() {
+	std::string &get_author() {
 		return author;
 	}
-	const std::string &get_copyright_date() {
+	std::string &get_copyright_date() {
 		return copyright_date;
+	}
+	Genre get_genre() {
+		return genre;
 	}
 
 private:
@@ -55,22 +44,11 @@ private:
 	std::string title;
 	std::string author;
 	std::string copyright_date;
+	Genre genre;
 };
 
-bool operator==(const Book &lhs, const Book &rhs) {
-	return lhs.get_isbn().compare(rhs.get_isbn()) == 0;
-}
+bool operator==(const Book &lhs, const Book &rhs);
 
-bool operator!=(const Book &lhs, const Book &rhs) {
-	return lhs.get_isbn().compare(rhs.get_isbn()) != 0;
-}
+bool operator!=(const Book &lhs, const Book &rhs);
 
-std::ostream &operator<<(std::ostream &os, const Book &rhs) {
-	os << rhs.get_title() << '\n';
-	os << rhs.get_author() << '\n';
-	os << rhs.get_isbn() << '\n';
-	os << std::endl;
-}
-
-enum class Genre { fiction, nonfiction, periodical, biography, children };
-git
+std::ostream &operator<<(std::ostream &os, const Book &rhs);
