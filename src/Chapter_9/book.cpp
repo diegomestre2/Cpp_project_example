7e01ba4314922a4e6b138c383751676ac65f98a9
diegomestre2 <diegogomest@gmail.com>
2020-07-01 19:58:22 +0200

    Adding more impl for book

--- src/Chapter_9/book.cpp
+++ src/Chapter_9/book.cpp
@@ -3,3 +3,36 @@
 int main(int argc, char **argv) {
 	Book book;
 }
+
+bool operator==(Book &lhs, Book &rhs) {
+	return lhs.get_isbn().compare(rhs.get_isbn()) == 0;
+}
+
+bool operator!=(Book &lhs, Book &rhs) {
+	return lhs.get_isbn().compare(rhs.get_isbn()) != 0;
+}
+
+std::ostream &operator<<(std::ostream &os, Book &rhs) {
+	os << rhs.get_title() << '\n';
+	os << rhs.get_author() << '\n';
+	os << rhs.get_isbn() << '\n';
+	os << std::endl;
+	return os;
+}
+
+bool Book::is_valid_isbn(std::string &isbn) {
+	size_t count = 0;
+	for (auto ch : isbn) {
+		count++;
+		if (ch != '-') {
+			auto digit = static_cast<uint32_t>(ch + '0');
+			if ((digit < 0 || digit > 9)) {
+				if (count == 4) {
+					return true;
+				}
+				return false;
+			}
+		}
+	}
+	return true;
+}
