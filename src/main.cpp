c067169fbc6a1bf6845668a0a5a32b22c2123e76
diegomestre2 <diegogomest@gmail.com>
2020-07-02 10:52:21 +0200

    Adding main

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
+#include <library.hpp>
 
-//! inline replaces the function definition whereever it is called and makes sure the function won't have a function call overhead.
-inline void keep_window_open() { 
-    char a{'\0'};
-    std::cin >> a;
-}
-
-/*int main(int argc, char** argv) {
-    std::cout << "Hello, World" << std::endl;
-    keep_window_open();
-    return 0;
-}*/
-
-/*
-Exercises
-
-1. Change the program to output the two lines
-Hello, programming!
-Here we go!
-*/
-
-int main(int argc, char** argv) {
-	std::cout << "Hello, Programming!" << '\n';
-	std::cout << "Here we go!" << std::endl;
+int main(int argc, char **argv) {
+	Library lib;
+	auto book1 = Book("1-2-3-A", "one up", "Peter Lynch", "02-07-2020", Genre::nonfiction);
+	auto book2 = Book("1-2-4-A", "learn to earn", "Peter Lynch", "02-07-2020", Genre::nonfiction);
+	lib.add_book(book1);
+	lib.add_book(book2);
+	auto patron1 = Patron("diego", 123);
+	auto patron2 = Patron("jessica", 124);
+	lib.add_patron(patron1);
+	lib.checkout_book(book1, patron1);
+	lib.checkout_book(book2, patron2);
 }
