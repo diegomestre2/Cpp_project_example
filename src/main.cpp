b613d3c1ae37cd77356cfaa396e456a045653ff4
diegomestre2 <diegogomest@gmail.com>
2020-07-02 19:50:45 +0200

    Adding point exerc

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-#include <library.hpp>
-#include <string>
+#include "library.hpp"
+#include "point.hpp"
 
-int main(int argc, char **argv) {
+#include <iostream>
+#include <string>
+#include <vector>
+
+void test_library() {
 	Library lib;
 	auto book1 = Book("1-2-3-A", "one up", "Peter Lynch", "02-07-2020", Genre::nonfiction);
 	auto book2 = Book("1-2-4-A", "learn to earn", "Peter Lynch", "02-07-2020", Genre::nonfiction);
@@ -15,4 +18,38 @@ int main(int argc, char **argv) {
 	lib.add_patron(patron1);
 	lib.checkout_book(patron1, book1);
 	lib.checkout_book(patron2, book2);
+}
+
+int main(int argc, char **argv) {
+	std::cout << "Give two coordinate members (x,y):" << '\n';
+	std::vector<Point> original_points;
+	std::cout << "Creating points" << '\n';
+	for (int x, y; std::cin >> x >> y;) {
+		auto point = Point(x, y);
+		original_points.push_back(point);
+		point.print();
+	}
+	std::cout << "Writing Points" << '\n';
+	auto file_name = std::string("mydata.txt");
+	std::ofstream output{file_name};
+	if (!output)
+		std::cout << "error on output file!" << std::endl;
+	for (auto &point : original_points) {
+		output << point;
+	}
+	std::cout << "Reading Points" << '\n';
+	std::ifstream input{file_name};
+	if (!input)
+		std::cout << "error on input file!" << std::endl;
+	std::vector<Point> processed_points;
+	char x, y;
+	while (true) {
+		if (!(input >> x >> y))
+			break;
+		processed_points.emplace_back(x, y);
+	}
+	std::cout << "Processed" << '\n';
+	for (auto &point : processed_points) {
+		point.print();
+	}
 }
