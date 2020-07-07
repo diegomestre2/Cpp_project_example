2eff9484cf2c26a10ac9bd93328bab160615c7ea
diegomestre2 <diegogomest@gmail.com>
2020-07-07 18:51:18 +0200

    Changing dir

---
+++ src/Chapter_10/main.cpp
@@ -0,0 +1,11 @@
+#include "file_append.cpp"
+#include "store_temps.cpp"
+#include "temp_stats.cpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char **argv) {
+	test_file_append();
+}
