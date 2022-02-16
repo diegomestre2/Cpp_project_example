cac4e377936e9de4a43b9eabadedc5103290271a
Diego Gomes Tomé <diegogomest@gmail.com>
2022-02-16 11:35:02 +0100

    Update

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_RUNNER
 #include "catch2/catch_all.hpp"
-
-#include <test_helpers.hpp>
+#include <iostream>
+#include "test_helpers.hpp"
 
 int main(int argc, char *argv[]) {
 	std::cout << "test" << std::endl;
